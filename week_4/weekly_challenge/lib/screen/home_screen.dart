bdf7fcfdce7bb31e4cd87272cd7bf110deb3e33f
wlgnsdl815 <wlgnsdl815@gmail.com>
2023-07-29 23:27:10 +0900

    도전과제 시작 전 코드 정리

--- week_4/weekly_challenge/lib/screen/home_screen.dart
+++ week_4/weekly_challenge/lib/screen/home_screen.dart
@@ -14,16 +14,18 @@ class HomeScreen extends StatefulWidget {
 
 class _SecondScreenState extends State<HomeScreen> {
   Dio dio = Dio();
-  RefreshController _refreshController = RefreshController();
   bool isRefreshing = false;
   bool isLoading = false;
+
+  final RefreshController _refreshController = RefreshController();
+  final PageController _pageController = PageController();
 
   void _onRefresh() async {
     setState(() {
       isRefreshing = true;
     });
 
-    await Future.delayed(Duration(seconds: 1));
+    await Future.delayed(Duration(seconds: 2));
 
     _refreshController.refreshCompleted();
 
@@ -32,149 +34,151 @@ class _SecondScreenState extends State<HomeScreen> {
     });
   }
 
+  void _connectionCheck() async {
+    setState(() {
+      isLoading = true;
+    });
+
+    await Future.delayed(Duration(seconds: 2), () async {
+      return await Connectivity().checkConnectivity();
+    });
+
+    setState(() {
+      isLoading = false;
+      _onRefresh();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          '도전하기!',
-          style: TextStyle(fontWeight: FontWeight.bold),
+        title: GestureDetector(
+          onTap: () {
+            _pageController.animateTo(
+              0,
+              duration: Duration(milliseconds: 300),
+              curve: Curves.easeIn,
+            );
+          },
+          child: Text(
+            '도전하기!',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
         ),
       ),
       body: isLoading
-          ? Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('인터넷 연결 확인중입니다.'),
-                  SizedBox(height: 10.0),
-                  // SpinKit 패키지 인디케이터 사용
-                  SpinKitFadingCircle(
-                    color: Colors.grey,
-                    size: 50.0,
-                  ),
-                ],
-              ),
-            )
+          ? _renderCircle()
           : isRefreshing
-              ? Shimmer.fromColors(
-                  baseColor: Colors.grey,
-                  highlightColor: Colors.grey[200]!,
-                  child: GridView.builder(
-                      itemCount: 5,
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 2,
-                      ),
-                      itemBuilder: (context, index) {
-                        return Padding(
-                          padding: const EdgeInsets.all(4.0),
-                          child: Card(
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(
-                              16.0,
-                            )),
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Column(
-                                children: [
-                                  Expanded(
-                                    child: Container(
-                                      color: Colors.white,
-                                    ),
-                                  ),
-                                  SizedBox(height: 8.0),
-                                  Container(
-                                    width: double.infinity,
-                                    height: 8.0,
-                                    color: Colors.white,
-                                  ),
-                                  Align(
-                                    alignment: Alignment.centerLeft,
-                                    child: Icon(
-                                      Icons.insert_comment,
-                                      color: Colors.grey,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                        );
-                      }),
-                )
-              : FutureBuilder(
-                  future: dio.get('https://sniperfactory.com/sfac/read_dogs'),
-                  builder: (context, snapshot) =>
-                      snapshot.connectionState == ConnectionState.done
-                          ? SmartRefresher(
-                              controller: _refreshController,
-                              onRefresh: _onRefresh,
-                              child: GridView.builder(
-                                gridDelegate:
-                                    SliverGridDelegateWithFixedCrossAxisCount(
-                                        crossAxisCount: 2),
-                                itemCount: 5,
-                                itemBuilder: (context, index) {
-                                  return Padding(
-                                    padding: const EdgeInsets.all(4.0),
-                                    child: Card(
-                                      shape: RoundedRectangleBorder(
-                                          borderRadius: BorderRadius.circular(
-                                        16.0,
-                                      )),
-                                      child: Padding(
-                                        padding: const EdgeInsets.all(8.0),
-                                        child: Column(
-                                          children: [
-                                            Expanded(
-                                              child: Image.network(snapshot
-                                                  .data!
-                                                  .data['body'][index]['url']),
-                                            ),
-                                            SizedBox(height: 8.0),
-                                            Text(
-                                              snapshot.data!.data['body'][index]
-                                                  ['msg'],
-                                              style: TextStyle(
-                                                  fontWeight: FontWeight.bold),
-                                            ),
-                                            Align(
-                                              alignment: Alignment.centerLeft,
-                                              child: Icon(
-                                                Icons.insert_comment,
-                                                color: Colors.grey,
-                                              ),
-                                            ),
-                                          ],
-                                        ),
-                                      ),
-                                    ),
-                                  );
-                                },
-                              ),
-                            )
-                          : Center(
-                              child: CircularProgressIndicator(),
-                            )),
+              ? _renderShimmer()
+              : _renderFutureBuild(),
       floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          setState(() {
-            isLoading = true;
-          });
-
-          await Future.delayed(Duration(seconds: 2), () async {
-            return await Connectivity().checkConnectivity();
-          });
-
-          setState(() {
-            isLoading = false;
-            _onRefresh();
-          });
-        },
+        onPressed: _connectionCheck,
         child: Icon(
           Icons.wifi_find,
         ),
       ),
     );
+  }
+
+  Center _renderCircle() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('인터넷 연결 확인중입니다.'),
+          SizedBox(height: 10.0),
+          // SpinKit 패키지 인디케이터 사용
+          SpinKitFadingCircle(
+            color: Colors.grey,
+            size: 50.0,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Shimmer _renderShimmer() {
+    return Shimmer.fromColors(
+      baseColor: Colors.grey,
+      highlightColor: Colors.grey[200]!,
+      child: GridView.builder(
+          itemCount: 10,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+          ),
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: Card(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(
+                  16.0,
+                )),
+              ),
+            );
+          }),
+    );
+  }
+
+  FutureBuilder<Response<dynamic>> _renderFutureBuild() {
+    return FutureBuilder(
+        future: dio.get('https://sniperfactory.com/sfac/read_dogs'),
+        builder: (context, snapshot) => snapshot.connectionState ==
+                ConnectionState.done
+            ? SmartRefresher(
+                header: MaterialClassicHeader(),
+                controller: _refreshController,
+                onRefresh: _onRefresh,
+                child: GridView.builder(
+                  controller: _pageController,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                  ),
+                  itemCount: snapshot.data!.data['body'].length,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: Card(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(
+                          16.0,
+                        )),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            children: [
+                              Expanded(
+                                child: Image.network(
+                                  // 시고르자브종은 이미지 불러오기가 안되어서 빈 화면을 보여준다.
+                                  snapshot.data!.data['body'][index]['url'],
+                                  errorBuilder: (context, error, stackTrace) {
+                                    return SizedBox();
+                                  },
+                                ),
+                              ),
+                              SizedBox(height: 8.0),
+                              Text(
+                                snapshot.data!.data['body'][index]['msg'],
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                              Align(
+                                alignment: Alignment.centerLeft,
+                                child: Icon(
+                                  Icons.insert_comment,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              )
+            : Center(
+                child: CircularProgressIndicator(),
+              ));
   }
 }
