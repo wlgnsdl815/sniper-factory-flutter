1a66bd7514587474103aabc69e2ff275ee9acb3e
wlgnsdl815 <wlgnsdl815@gmail.com>
2023-08-05 01:51:53 +0900

    두번째 프로젝트 생성

---
+++ week_5/weekly_assessment/lib/second_assignment/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:weekly_assessment/second_assignment/screens/home.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
