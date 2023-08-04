7460cfeb330fa9ad86d04517c1a9b6d1ce296ea2
wlgnsdl815 <wlgnsdl815@gmail.com>
2023-08-04 22:11:41 +0900

    첫번째 과제 준비

---
+++ week_5/weekly_assessment/lib/first_assignment/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:weekly_assessment/first_assignment/screens/home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
