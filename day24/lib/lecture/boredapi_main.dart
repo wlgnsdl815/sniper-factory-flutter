975e7b4e93673e5c131fbb5f9b6564de39d815e3
wlgnsdl815 <wlgnsdl815@gmail.com>
2023-08-07 15:46:26 +0900

    boredapi done

---
+++ day24/lib/lecture/boredapi_main.dart
@@ -0,0 +1,57 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
+
+class BoredApi {
+  String activity;
+  String type;
+  int participants;
+  double price;
+  String? link;
+  String key;
+  double accessibility;
+
+  BoredApi({
+    required this.activity,
+    required this.type,
+    required this.participants,
+    required this.price,
+    this.link,
+    required this.key,
+    required this.accessibility,
+  });
+
+  factory BoredApi.fromJson(Map<String, dynamic> json) {
+    return BoredApi(
+      activity: json['activity'],
+      type: json['type'],
+      participants: json['participants'],
+      price: json['price'] is int
+          ? double.parse(json['price'].toString())
+          : json['price'],
+      link: json['link'] ?? '',
+      key: json['key'],
+      accessibility: json['accessibility'],
+    );
+  }
+  @override
+  String toString() =>
+      'BoredApi: (activity: $activity, type: $type, participants: $participants, price: $price, link: $link,key: $key, accessibility: $accessibility)';
+}
+
+void main() async {
+  try {
+    Dio dio = Dio();
+    String url = 'https://www.boredapi.com/api/activity';
+    Response<Map<String, dynamic>> response = await dio.get(url);
+    if (response.statusCode == 200) {
+      print(response.data!['price'].runtimeType);
+
+      BoredApi result = BoredApi.fromJson(response.data!);
+      print(result);
+    }
+  } catch (e) {
+    throw Exception(e);
+  }
+}
