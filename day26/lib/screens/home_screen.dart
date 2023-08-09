d2761efa2cf28c8bbe0c0828b48e3b5169dbaa81
wlgnsdl815 <wlgnsdl815@gmail.com>
2023-08-09 12:33:46 +0900

    AppBar blur 추가

--- day26/lib/screens/home_screen.dart
+++ day26/lib/screens/home_screen.dart
@@ -1,3 +1,9 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables
+
+import 'dart:ui';
+import 'package:day26/components/todo_card.dart';
+import 'package:day26/models/todo_model.dart';
+import 'package:day26/services/todo_servive.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -6,12 +12,46 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        foregroundColor: Colors.black,
-        centerTitle: false,
-        title: Text('Todo App'),
-        backgroundColor: Colors.transparent,
-        elevation: 0.0,
+      extendBodyBehindAppBar: true,
+      appBar: PreferredSize(
+        preferredSize: Size(MediaQuery.of(context).size.width, kToolbarHeight),
+        child: ClipRRect(
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+            child: AppBar(
+              centerTitle: false,
+              elevation: 0.0,
+              backgroundColor: Colors.transparent,
+              foregroundColor: Colors.black,
+              title: Text('Todo App'),
+              actions: [
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.filter_list),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.refresh),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      body: FutureBuilder(
+        future: TodoService().getData(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                Todo todo = snapshot.data![index];
+                return TodoCard(todo: todo);
+              },
+            );
+          }
+          return Center(child: CircularProgressIndicator());
+        },
       ),
     );
   }
