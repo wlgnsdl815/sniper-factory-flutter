0ab8553919c67388c36ef807b3c18a4f9665f567
wlgnsdl815 <wlgnsdl815@gmail.com>
2023-08-10 19:34:05 +0900

    riverpod 예시 프로젝트 생성.

---
+++ widgets_examples/riverp/README.md
@@ -0,0 +1,5 @@
+# Riverpod
+
+폴더이름에 riverpod이 들어가면 pub get이 안되기때문에 riverp로 생성
+
+## Riverpod 예시 폴더
---
+++ widgets_examples/riverp/android/app/src/main/kotlin/com/example/riverp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.riverp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
